654852bebec434fd8d773995d23894e7f5ffc798
mdbelalhosan5432 <mdbelalhosan5432@gmail.com>
2026-04-10 19:01:08 +0600

    Refactor service worker install and fetch event handlers

--- sw.js
+++ sw.js
@@ -4,40 +4,17 @@ const urlsToCache =;
 // Install Service Worker
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Fetch resources
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
-
-// Update Service Worker
-self.addEventListener('activate', event => {
-  const cacheWhitelist =;
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
-
